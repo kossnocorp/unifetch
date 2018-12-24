b519f83706c41114af54589b9ed37ad512b45fde
Sasha Koss <koss@nocorp.me>
2018-12-24 11:39:26 +0530

    Export Request, RequestInfo and RequestInit types

--- browser.d.ts
+++ browser.d.ts
@@ -1,1 +1,9 @@
 export default window.fetch
+type _Request = Request
+type _RequestInit = RequestInit
+type _RequestInfo = RequestInfo
+export {
+  _Request as Request,
+  _RequestInit as RequestInit,
+  _RequestInfo as RequestInfo
+}
--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 import fetch from 'node-fetch'
+export { Request, RequestInfo, RequestInit } from 'node-fetch'
 export default fetch
